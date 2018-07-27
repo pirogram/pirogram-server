b2a929b5c6686a129822fb8c8993c14e7f1350a2
Manas Garg <manasgarg@gmail.com>
2018-07-26 17:09:46 -0700

    ipython html output handling

--- client/components/code-playground/code-output.jsx
+++ client/components/code-playground/code-output.jsx
@@ -13,11 +13,14 @@ export default class CodeOutput extends React.Component {
                     if( item.name == 'stdout' || item.name == 'stderr') {
                         return <pre>{item.text}</pre>
                     } else if( item.name == 'object') {
+                        console.log(item.data);
                         if( item.data['image/png']) {
                             return <img className='ui image' src={'data:image/png;base64,' + item.data['image/png']}/>
+                        } else if( item.data['text/html']) {
+                            const tableHtml = item.data['text/html'].replace('<table border="1" class="dataframe">', '<table class="ui celled table">');
+                            return <div className='ipython-html'>{Parser(tableHtml)}</div>
                         }
                     } else if( item.name == 'traceback') {
-                        console.log()
                         return <pre className='traceback'>{Parser(item.data)}</pre>
                     }
                 })}
--- filehashes.js
+++ filehashes.js
@@ -1,1 +1,1 @@
-module.exports = {client_bundle_hash: "e13bdc06a2d934d3494a0c2fdf2ac86be6cad1d0"};
+module.exports = {client_bundle_hash: "58f40572b7b8f9519e4aaf725fc7c59508ea72d1"};
